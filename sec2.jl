36d7c567a7a41be3e296e35b84cc9e15079e5afe
YokoPhys-h <yoko.haruru.phy@outlook.jp>
2022-06-15 23:54:59 +0900

    update

--- sec2.jl
+++ sec2.jl
@@ -58,3 +58,34 @@ end
 
 x = 100;
 x > 100 ? true : false
+
+n = 100;
+
+## ループ
+i = 1;
+
+while i <= 5
+    println(i)
+    global i += 1
+end
+
+for i in 1:5
+    println(i)
+end
+
+function add(x, y)
+    return x + y
+end
+
+add_typed(x::Int, y::Int)::Float64 = x + y;
+add_typed(1, 2)
+
+function sum_diff(x, y)
+    return (x + y, x - y)
+end
+sum_diff(1, 4)
+
+list = []
+list = [1, 2]
+push!(list, 3)
+pop!(list)
